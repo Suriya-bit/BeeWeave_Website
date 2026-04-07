5ee17481a09d5f240f2644deb669ffe615439c9d
Prakash-WebDad <prakash.s@webdads2u.com>
2026-04-07 16:19:13 +0530

    font-family and root varibles update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 import SmoothScrollProvider from "./common/SmoothScrollProvider";
 import Footer from "./components/Common/Footer/Footer";
 import Header from "./components/Common/Header/Header";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+
 
 export const metadata = {
   title: "BeeWeave - Your Ultimate AI Resource Hub in 2025",
@@ -9,10 +11,16 @@ export const metadata = {
     "Discover the best AI tools, resources, and insights at BeeWeave. Your ultimate destination for all things AI in 2025.",
 };
 
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  display: "swap",
+  variable: "--font-jakarta",
+});
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>
+      <body className={jakarta.variable} suppressHydrationWarning={true}>
         <SmoothScrollProvider>
           <Header />
           <main>{children}</main>

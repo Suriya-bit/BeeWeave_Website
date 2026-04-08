c61b40d3cc85c041cf87b1d03bda0eecb7a150b7
Prakash-WebDad <prakash.s@webdads2u.com>
2026-04-08 17:00:50 +0530

    react icons update

--- app/components/Container/HomeSection/Industriesfaq.jsx
+++ app/components/Container/HomeSection/Industriesfaq.jsx
@@ -2,29 +2,29 @@
 import { useRef } from "react";
 import MainLayout from "@/app/common/MainLayout";
 import { industries } from "@/app/utils/HomemockData";
-import linear from "@/app/assets/Images/award-linear.svg";
-import Image from "next/image";
-import { motion, useAnimationFrame } from "framer-motion";
+import { useAnimationFrame } from "framer-motion";
 
-const Item = ({ label }) => (
-  <div className="flex flex-col items-center gap-3 min-w-[121px]">
-    <div className="w-16 h-16 rounded-full flex items-center justify-center border border-purple-700/50 bg-purple-900/20">
-      <Image src={linear} alt="icon" width={28} height={28} />
+const Item = ({ label, icon }) => (
+  <div className="flex flex-col items-center gap-3 min-w-[140px] group">
+    <div className="w-16 h-16 rounded-full flex items-center justify-center border border-purple-700/50 bg-purple-900/20 transition-all duration-300  group-hover:bg-purple-800/40">
+      {icon}
     </div>
-    <span className="text-md text-white font-medium text-center">
+    <span className="text-sm  text-white font-medium text-center">
       {label}
     </span>
   </div>
 );
 
+
 export default function IndustriesFaq() {
   const x = useRef(0);
   const containerRef = useRef(null);
   useAnimationFrame((t, delta) => {
-    const speed = 0.05;
+    const speed = 0.06;
     x.current -= speed * delta;
     if (containerRef.current) {
       const width = containerRef.current.scrollWidth / 2;
+
       if (Math.abs(x.current) >= width) {
         x.current = 0;
       }
@@ -35,13 +35,20 @@ export default function IndustriesFaq() {
   return (
     <div className="text-white font-sans">
       <MainLayout className="py-20 px-4">
-        <h2 className="text-center text-xl md:text-5xl font-semibold mb-14">
+        <h2 className="text-center text-2xl md:text-5xl font-semibold mb-14">
           Industries We Serve
         </h2>
         <div className="overflow-hidden w-full">
-          <div ref={containerRef} className="flex gap-6 w-max">
-            {[...industries, ...industries, ...industries].map((item, index) => (
-              <Item key={index} label={item.label} />
+          <div
+            ref={containerRef}
+            className="flex gap-10 w-max will-change-transform"
+          >
+            {[...industries, ...industries].map((item, index) => (
+              <Item
+                key={index}
+                label={item.label}
+                icon={item.icon}
+              />
             ))}
           </div>
         </div>

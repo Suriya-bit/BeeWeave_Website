64c3b0c41a209d34d844d0e3813eaac04d47dad7
Prakash-WebDad <prakash.s@webdads2u.com>
2026-04-17 12:01:33 +0530

    robots text add

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,11 +2,13 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "./LayoutWrapper/LayoutWrapper";
 
-
 export const metadata = {
   title: "BeeWeave - Your Ultimate AI Resource Hub in 2025",
   description:
     "Discover the best AI tools, resources, and insights at BeeWeave. Your ultimate destination for all things AI in 2025.",
+  verification: {
+    google: "De0yegm7cjCpN-tRgB5pCmB4QN7ms4PQIMuSuLYivXw",
+  },
 };
 
 const jakarta = Plus_Jakarta_Sans({
@@ -15,11 +17,11 @@ const jakarta = Plus_Jakarta_Sans({
   display: "swap",
   variable: "--font-jakarta",
 });
+
 export default function RootLayout({ children }) {
-
-
   return (
     <html lang="en">
+      <head />
       <body className={jakarta.variable} suppressHydrationWarning={true}>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>

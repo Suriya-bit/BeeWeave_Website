428ff6de465df10584d9a7fbf1e83b5d15f2a84a
Prakash-WebDad <prakash.s@webdads2u.com>
2026-05-06 18:06:20 +0530

    goggle tage added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import LayoutWrapper from "./LayoutWrapper/LayoutWrapper";
 
@@ -21,8 +22,34 @@ const jakarta = Plus_Jakarta_Sans({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body className={jakarta.variable} suppressHydrationWarning={true}>
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P8H32CRL');
+          `}
+        </Script>
+      </head>
+
+      <body
+        className={jakarta.variable}
+        suppressHydrationWarning={true}
+      >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P8H32CRL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
